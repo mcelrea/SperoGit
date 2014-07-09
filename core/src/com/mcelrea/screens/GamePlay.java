f4c46dc45b6184e26e4d9d250073e39dab4388ed
mcelrea <mcelrea@damien-hs.edu>
2014-07-09 11:21:24 -0700

    Test commit

--- core/src/com/mcelrea/screens/GamePlay.java
+++ core/src/com/mcelrea/screens/GamePlay.java
@@ -31,7 +31,7 @@ public class GamePlay implements Screen{
 
 	@Override
 	public void render(float delta) {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
 		world.step(TIMESTEP, VELOCITYITERATIONS, POSITIONITERATIONS);
